aec26723b92cdde9e7520eff00900b0b4e139ede
kanchi-22 <76424618+kanchi-22@users.noreply.github.com>
2021-06-01 17:23:30 +0530

    Fixed a bug
    The result for searching a particular word's occurrence wasn't correct if the searched word ended with a punctuation mark. Made some changes to output the perfect result.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("Welcome to the console of Text Analyzer Website");
 let text_entered;
 text_entered = document.getElementById("input-text");
 let text = "";
+// declaring the document element variables-->
 let clear_button = document.getElementById("clear-button");
 let go_button = document.getElementById("go-button");
 let search_button = document.getElementById("search-button");
@@ -11,6 +12,7 @@ let punctuationCount = document.getElementById("punctuation-count");
 let cletterCount = document.getElementById("cLetter-count");
 let searchResult = document.getElementById("search-result");
 let searchInput = document.getElementById("keyword");
+// when the go button is clicked-->
 go_button.addEventListener('click',function(){
     text = text_entered.value;
     let l = text.length;
@@ -40,6 +42,7 @@ go_button.addEventListener('click',function(){
     punctuationCount.innerText = `${punctuations} punctuations.`;
     cletterCount.innerText = `${cLetter} capital letters.`;
 })
+// when the clear button is clicked-->
 clear_button.addEventListener('click',function(){
 
     text_entered.value="";
@@ -49,25 +52,33 @@ clear_button.addEventListener('click',function(){
     cletterCount.innerText = "";
 
 })
+// when the search button is clicked-->
 search_button.addEventListener('click',function(){
     let text = text_entered.value;
+    text_lc = text.toLowerCase();
     let keyword = searchInput.value;
-    if(text == ""){
+    keyword_lc = keyword.toLowerCase();
+    if(text_lc == ""){
         return;
     }
-    let arr = text.split(' ');
+    let arr = text_lc.split(' ');
     let count = 0;
     let i;
     for(i = 0 ; i < arr.length ; i++){
-        if(arr[i] == keyword){
+        if(arr[i] == keyword_lc){
             count++;
+        }
+        else if(arr[i][arr[i].length-1] == '.' || arr[i][arr[i].length-1] == ',' || arr[i][arr[i].length-1] == '?' || arr[i][arr[i].length-1] == '!'){
+            if(arr[i].substr(0,arr[i].length-1) == keyword_lc){
+                count++;
+            }
         }
     }
     if(count == 0){
-        searchResult.innerText = `This text does not contain the word'${keyword}'.`;
+        searchResult.innerText = `This text does not contain the word'${keyword_lc}'.`;
     }
     else{
-        searchResult.innerText = `This text contains '${keyword}' ${count} times.`;
+        searchResult.innerText = `This text contains '${keyword_lc}' ${count} times.`;
     }
     searchInput.value = "";
 })
